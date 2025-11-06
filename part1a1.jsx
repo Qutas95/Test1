a7f99715a25832620099984b41378bc064e2d3b6
Rafal Walczynski <147498292+Qutas95@users.noreply.github.com>
2025-11-06 13:17:25 +0100

    Update part1a1.jsx

--- part1a1.jsx
+++ part1a1.jsx
@@ -1,9 +1,11 @@
+// Komponent Header wyświetla tytuł kursu przekazany przez props
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.course}</h1> 
   )
 }
 
+// Komponent Content wyświetla trzy części kursu wraz z liczbą ćwiczeń
 const Content = (props) => {
   return (
     <div>
@@ -14,12 +16,16 @@ const Content = (props) => {
   )
 }
 
+// Komponent Total oblicza i wyświetla łączną liczbę ćwiczeń
 const Total = (props) => {
   return (
-    <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    <p>
+      Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
   )
 }
 
+// Główny komponent aplikacji
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -28,15 +34,17 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
+//przekazanie nazwy kursu, danych o częściach kursu i liczby ćwiczeń do odpowiadajacych im komponentów
   return (
     <div>
       <Header course={course} />
+
       <Content
         part1={part1} exercises1={exercises1}
         part2={part2} exercises2={exercises2}
         part3={part3} exercises3={exercises3}
       />
+
       <Total
         exercises1={exercises1}
         exercises2={exercises2}
