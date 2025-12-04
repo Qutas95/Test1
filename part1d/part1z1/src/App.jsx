4ec76b4e09f87e89532942ad4088c09635d36a5e
Rafał Walczyński <walczynskir@tlimc.local>
2025-12-04 12:32:21 +0100

    zadanie 1.8

--- part1d/part1z1/src/App.jsx
+++ part1d/part1z1/src/App.jsx
@@ -1,42 +1,60 @@
 import { useState } from 'react'
 
+
+const Statistics = (props) => {
+    const { good, neutral, bad } = props
+    const total = good + neutral + bad
+    const average = total === 0 ? 0 : (good - bad) / total
+    const positive = total === 0 ? 0 : (good / total) * 100
+
+    
+
+    return (
+        <>
+            <h1>Statistics</h1>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <p>total {total}</p>
+            <p>average {average}</p>
+            <p>positive {positive} %</p>
+        </>
+    )
+}
+
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total2 = good+bad+neutral
-  const [total, setTotal] = useState(0)
-  const positive = total2 === 0 ? 0 : (good / total2) * 100
-  const average = total2 === 0 ? 0 : (good - bad) / total2
+
   const handlegoodClick = () => {
     setGood(good + 1)
-    setTotal(good+neutral-bad)
-      }
+  }
 
   const handleneutralClick = () => {
     setNeutral(neutral + 1)
-    setTotal(good+neutral-bad)
-      }
+  }
+
   const handlebadClick = () => {
-    setBad(bad + 1)
-    setTotal(good+neutral-bad)  
-      }
+    setBad(bad+ 1)
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={handlegoodClick}>good</button>
       <button onClick={handleneutralClick}>neutral</button>
       <button onClick={handlebadClick}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
- 
-      <p>total {total}</p>
-      <p>avarage {average}</p>
-      <p>positive {positive} %</p>
-         </div>
+      
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
   )
 }
 
 export default App
+
+
+
+
+
