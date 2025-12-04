c680515733268923f0efe8138c46019f259bb6ac
Rafał Walczyński <walczynskir@tlimc.local>
2025-12-04 12:41:36 +0100

    zadanie 1.10

--- part1d/part1z1/src/App.jsx
+++ part1d/part1z1/src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
 
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+)
 
 const Statistics = (props) => {
     const { good, neutral, bad } = props
     const total = good + neutral + bad
-    const average = total === 0 ? 0 : (good - bad) / total
-    const positive = total === 0 ? 0 : (good / total) * 100
 
-  
+    const average = total === 0 ? 0 : ((good - bad) / total)
+    const positive = total === 0 ? 0 : ((good / total) * 100)
+
     if (total === 0) {
       return (
           <>
@@ -15,19 +28,23 @@ const Statistics = (props) => {
               <p>No feedback given</p>
           </>
       )
-  }
+    }
 
-  return (
-      <>
-          <h1>Statistics</h1>
-          <p>good {good}</p>
-          <p>neutral {neutral}</p>
-          <p>bad {bad}</p>
-          <p>total {total}</p>
-          <p>average {average}</p>
-          <p>positive {positive} %</p>
-      </>
-  )
+    return (
+        <>
+            <h1>Statistics</h1>
+            <table>
+                <tbody>
+                    <StatisticLine text="good" value={good} />
+                    <StatisticLine text="neutral" value={neutral} />
+                    <StatisticLine text="bad" value={bad} />
+                    <StatisticLine text="total" value={total} />
+                    <StatisticLine text="average" value={average} />
+                    <StatisticLine text="positive" value={positive + " %"} />
+                </tbody>
+            </table>
+        </>
+    )
 }
 
 
@@ -36,24 +53,17 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const handlegoodClick = () => {
-    setGood(good + 1)
-  }
 
-  const handleneutralClick = () => {
-    setNeutral(neutral + 1)
-  }
-
-  const handlebadClick = () => {
-    setBad(bad+ 1)
-  }
+  const handlegoodClick = () => setGood(good + 1)
+  const handleneutralClick = () => setNeutral(neutral + 1)
+  const handlebadClick = () => setBad(bad + 1)
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={handlegoodClick}>good</button>
-      <button onClick={handleneutralClick}>neutral</button>
-      <button onClick={handlebadClick}>bad</button>
+      <Button handleClick={handlegoodClick} text="good" />
+      <Button handleClick={handleneutralClick} text="neutral" />
+      <Button handleClick={handlebadClick} text="bad" />
       
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
@@ -61,8 +71,3 @@ const App = () => {
 }
 
 export default App
-
-
-
-
-
