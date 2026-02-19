c41c39dd8d5b1077259b0910df853acca5769d1b
Rafał Walczyński <walczynskir@tlimc.local>
2026-02-19 12:39:41 +0100

    zadanie 2.1a

--- part1d/part1z1/src/App.jsx
+++ part1d/part1z1/src/App.jsx
@@ -27,11 +27,7 @@ const Statistics = (props) => {
         <>
             <h1>Statistics</h1>
           <table>
-
-
                 <tbody>
-
-
                     <StatisticLine text="good" value={good} />
                     <StatisticLine text="neutral" value={neutral} />
                     <StatisticLine text="bad" value={bad} />
@@ -44,6 +40,7 @@ const Statistics = (props) => {
     )
 }
 const App = () => {
+  const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
 
   const [bad, setBad] = useState(0)
@@ -63,7 +60,13 @@ const App = () => {
     <div>
 
       <h1>give feedback</h1>
+      <Button handleClick={handlegoodClick} text="good" />
 
+
+<Button handleClick={handleneutralClick} text="neutral" />
+
+
+<Button handleClick={handlebadClick} text="bad" />
 
       <Statistics good={good} neutral={neutral} bad={bad} />
 
