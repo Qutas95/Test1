75313d0b59dc83663df4a6182f26ae12220d3c32
Rafał Walczyński <walczynskir@tlimc.local>
2026-02-19 13:30:56 +0100

    zadanie 2c

--- part2/part2a/part1z1/src/App.jsx
+++ part2/part2a/part1z1/src/App.jsx
@@ -16,12 +16,15 @@ const Content = ({ parts }) => (
   </div>
 )
 
-// Nowy komponent Total wykorzystujący metodę reduce
 const Total = ({ parts }) => {
-  const sum = parts.reduce((s, p) => s + p.exercises, 0)
   
+  const total = parts.reduce((s, p) => {
+    console.log('what is happening', s, p) 
+    return s + p.exercises
+  }, 0) 
+
   return (
-    <p><b>total of {sum} exercises</b></p>
+    <p><b>total of {total} exercises</b></p>
   )
 }
 
@@ -40,26 +43,10 @@ const App = () => {
     id: 1,
     name: 'Half Stack application development',
     parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
+      { name: 'Fundamentals of React', exercises: 10, id: 1 },
+      { name: 'Using props to pass data', exercises: 7, id: 2 },
+      { name: 'State of a component', exercises: 14, id: 3 },
+      { name: 'Redux', exercises: 11, id: 4 }
     ]
   }
 
