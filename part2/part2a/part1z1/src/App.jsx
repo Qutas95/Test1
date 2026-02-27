8506cc31fdf4d1a3fbb0fbb5c51556a2c3ccdf5d
Rafał Walczyński <walczynskir@tlimc.local>
2026-02-27 12:37:37 +0100

    zadanie 2.4

--- part2/part2a/part1z1/src/App.jsx
+++ part2/part2a/part1z1/src/App.jsx
@@ -17,11 +17,7 @@ const Content = ({ parts }) => (
 )
 
 const Total = ({ parts }) => {
-  
-  const total = parts.reduce((s, p) => {
-    console.log('what is happening', s, p) 
-    return s + p.exercises
-  }, 0) 
+  const total = parts.reduce((s, p) => s + p.exercises, 0)
 
   return (
     <p><b>total of {total} exercises</b></p>
@@ -39,18 +35,57 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      { name: 'Fundamentals of React', exercises: 10, id: 1 },
-      { name: 'Using props to pass data', exercises: 7, id: 2 },
-      { name: 'State of a component', exercises: 14, id: 3 },
-      { name: 'Redux', exercises: 11, id: 4 }
-    ]
-  }
-
-  return <Course course={course} />
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+  return (
+    <div>
+      {courses.map(course => 
+        <Course key={course.id} course={course} />
+      )}
+    </div>
+  )
 }
 
 export default App
